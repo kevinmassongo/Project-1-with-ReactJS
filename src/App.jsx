2a8e291677a0b6d007b5b3f1086615a29643432a
Kevin Massongo <kevinmassongo196@gmail.com>
2023-11-16 15:10:54 +0100

    add codes

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,33 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue('X');
-  }
-
-  return (
-    <button className="square" onClick={handleClick}>{value}</button>
-  );
+function Square({value, onSquareClick}) {
+  return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
 
 function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  function handleClick() {
+    const nextSquares = squares.slice();
+    nextSquares[0] = "X";
+    setSquares(nextSquares);
+  }
   return (
     <>
     <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={Square[0]} onSquareClick={handleClick}/>
+        <Square value={Square[1]}/>
+        <Square value={Square[2]}/>
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={Square[3]}/>
+        <Square value={Square[4]}/>
+        <Square value={Square[5]}/>
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={Square[6]}/>
+        <Square value={Square[7]}/>
+        <Square value={Square[8]}/>
       </div>
     </>
     
